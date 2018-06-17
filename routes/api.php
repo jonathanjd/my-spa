5f47c65300a766eceb8519b6c11006061376882b
Jonathan Duran <headjd@gmail.com>
2018-06-17 01:23:29 -0400

    Like and DisLike

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{replay}', 'LikeController@likeit');
+Route::delete('/like/{replay}', 'LikeController@unlikeit');
