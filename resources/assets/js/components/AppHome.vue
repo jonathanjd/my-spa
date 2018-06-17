f2578e5873b0e6fb1f367be81c925769c1578de5
Jonathan Duran <headjd@gmail.com>
2018-06-17 11:00:48 -0400

    Vuetify + Router + Init  Component

---
+++ resources/assets/js/components/AppHome.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <app-toolbar></app-toolbar>
+  <app-footer></app-footer>
+</div>
+</template>
+
+<script>
+import AppToolbar from './AppToolbar';
+import AppFooter from './AppFooter';
+export default {
+
+  components: {
+    appToolbar: AppToolbar,
+    appFooter: AppFooter
+  }
+
+}
+</script>
+
+<style>
+</style>
