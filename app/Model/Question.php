19df3c110855b8b26985fd3df211fe19ea1ef18c
Jonathan Duran <headjd@gmail.com>
2018-06-16 21:50:38 -0400

    Relaciones

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+    public function user()
+    {
+        # code...
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        # code...
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        # code...
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+    public function question()
+    {
+        # code...
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        # code...
+        return $this->belongsTo(User::class);
+    }
+    public function like()
+    {
+        # code...
+        return $this->hasMany(Like::class);
+    }
 }
